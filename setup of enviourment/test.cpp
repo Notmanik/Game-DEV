b7e3d4fd1cd19eec30a098dc6edee03811294123
MANIK <98070699+Notmanik@users.noreply.github.com>
2025-04-09 12:47:01 +0530

    Made a circle in CPP and done with the skeleton of game class

--- setup of enviourment/test.cpp
+++ setup of enviourment/test.cpp
@@ -1,18 +1,53 @@
 #include <SFML/Graphics.hpp>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML + Linux + VSC");
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear(sf::Color::Black);
-        window.display();
+class Game{
+    public:
+        Game();
+        void run();
+    private:
+        void processEvents();
+        void update();
+        void render();
+    private:
+        sf::RenderWindow mWindow;
+        sf::CircleShape mPlayer;
+};
+// here all the functions defeinition as Game::run(){} 
+Game::Game()
+: mWindow(sf::VideoMode(640, 480), "SFML Application") // Create the main window
+, mPlayer() // Default-constructed player
+{
+    mWindow.setFramerateLimit(60);
+    mPlayer.setRadius(40.f);  // Set player size
+    mPlayer.setPosition(100.f, 100.f);  // Set player position
+    mPlayer.setFillColor(sf::Color::Cyan);  // Set player color
+}
+void Game::run(){
+    while(mWindow.isOpen()){
+        processEvents();
+        update();
+        render();
+        
     }
+}
+void Game::processEvents()
+{
+sf::Event event;
+while (mWindow.pollEvent(event))
+{
+if (event.type == sf::Event::Closed)
+mWindow.close();
+}
+}
+void Game::update(){
 
-    return 0;
+}
+void Game::render(){
+    mWindow.clear();
+    mWindow.draw(mPlayer);
+    mWindow.display();
+}
+int main(){
+    Game game;
+    game.run();
 }

3d0d56ca3c3023d98b53c50278c4ef131f82ecc1
MANIK <98070699+Notmanik@users.noreply.github.com>
2025-04-09 19:32:53 +0530

    implemented Movement

--- setup of enviourment/test.cpp
+++ setup of enviourment/test.cpp
@@ -1,53 +1,102 @@
 #include <SFML/Graphics.hpp>
 
-class Game{
-    public:
-        Game();
-        void run();
-    private:
-        void processEvents();
-        void update();
-        void render();
-    private:
-        sf::RenderWindow mWindow;
-        sf::CircleShape mPlayer;
-};
-// here all the functions defeinition as Game::run(){} 
-Game::Game()
-: mWindow(sf::VideoMode(640, 480), "SFML Application") // Create the main window
-, mPlayer() // Default-constructed player
+class Game
 {
-    mWindow.setFramerateLimit(60);
-    mPlayer.setRadius(40.f);  // Set player size
-    mPlayer.setPosition(100.f, 100.f);  // Set player position
+   public:
+    Game();
+    void run();
+
+   private:
+    void processEvents();
+    void update();
+    void render();
+    void handlePlayerInput(sf::Keyboard::Key key, bool ispressed);
+
+   private:
+    sf::RenderWindow mWindow;
+    sf::CircleShape mPlayer;
+    bool mIsMovingUp, mIsMovingDown, mIsMovingRight, mIsMovingLeft;
+};
+// here all the functions defeinition as Game::run(){}
+Game::Game()
+    : mWindow(sf::VideoMode(640, 480), "SFML Application")  // Create the main window
+      ,
+      mPlayer()  // Default-constructed player
+{
+    mWindow.setFramerateLimit(60);          // sets the frames cap to 60 so that it wont run without bounds
+    mPlayer.setRadius(40.f);                // Set player size
+    mPlayer.setPosition(100.f, 100.f);      // Set player position
     mPlayer.setFillColor(sf::Color::Cyan);  // Set player color
 }
-void Game::run(){
-    while(mWindow.isOpen()){
+
+void Game::run()
+{
+    while (mWindow.isOpen())
+    {
         processEvents();
         update();
         render();
-        
+    }
+}
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool ispressed)
+{
+    if (key == sf::Keyboard::W)
+    {
+        mIsMovingUp = ispressed;
+    }
+    if (key == sf::Keyboard::A)
+    {
+        mIsMovingLeft = ispressed;
+    }
+    if (key == sf::Keyboard::S)
+    {
+        mIsMovingDown = ispressed;
+    }
+    if (key == sf::Keyboard::D)
+    {
+        mIsMovingRight = ispressed;
     }
 }
 void Game::processEvents()
 {
-sf::Event event;
-while (mWindow.pollEvent(event))
+    sf::Event event;
+    while (mWindow.pollEvent(event))
+    {
+        switch (event.type)
+        {
+            case sf::Event::KeyPressed:
+                handlePlayerInput(event.key.code, true);
+                break;
+            case sf::Event::KeyReleased:
+                handlePlayerInput(event.key.code, false);
+                break;
+            case sf::Event::Closed:
+                mWindow.close();
+                break;
+        }
+    }
+}
+void Game::update()
 {
-if (event.type == sf::Event::Closed)
-mWindow.close();
+    sf::Vector2f movement(0.f, 0.f);
+    if (mIsMovingUp)
+        movement.y -= 1.f;
+    if (mIsMovingDown)
+        movement.y += 1.f;
+    if (mIsMovingLeft)
+        movement.x -= 1.f;
+    if (mIsMovingRight)
+        movement.x += 1.f;
+    mPlayer.move(movement);
 }
-}
-void Game::update(){
-
-}
-void Game::render(){
+void Game::render()
+{
     mWindow.clear();
     mWindow.draw(mPlayer);
     mWindow.display();
 }
-int main(){
+int main()
+{
     Game game;
     game.run();
 }
